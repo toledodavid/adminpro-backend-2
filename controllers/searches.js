9840b36e81dda6fa5bad8d41df21135a04e6024e
David Toledo <dtoledoz0207@gmail.com>
2023-03-27 13:26:43 -0600

    Add functionality to search a term in User, Doctor, Hospital models and return the result

--- controllers/searches.js
+++ controllers/searches.js
@@ -1,9 +1,26 @@
 const {request: req, response: res} = require('express');
+const Doctor = require('../models/doctor');
+const Hospital = require('../models/hospital');
+const User = require('../models/user');
 
-const getAll = (request = req, response = res) => {
+
+const getAll = async (request = req, response = res) => {
+
+  const target = request.params.target;
+  const regex = RegExp(target, 'i');
+
+
+  const [users, doctors, hospitals] = await Promise.all([
+    User.find({name: regex}),
+    Doctor.find({name: regex}),
+    Hospital.find({name: regex})
+  ]);
+
   response.json({
     ok: true,
-    message: request.params.target
+    users,
+    doctors,
+    hospitals
   });
 }
 
